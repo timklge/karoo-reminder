99ece6250125d7911b5d6c736afbac976706368b
Tim Kluge <timklge@gmail.com>
2025-02-02 18:45:04 +0100

    Further increase tone frequencies

--- app/src/main/kotlin/de/timklge/karooreminder/screens/Reminder.kt
+++ app/src/main/kotlin/de/timklge/karooreminder/screens/Reminder.kt
@@ -13,17 +13,26 @@ import kotlinx.serialization.json.Json
 @Serializable
 enum class ReminderBeepPattern(val displayName: String, val tones: List<PlayBeepPattern.Tone>) {
     NO_TONES("No tones", emptyList()),
-    THREE_TONES_UP("Three tones up", listOf(PlayBeepPattern.Tone(2_000, 200), PlayBeepPattern.Tone(2_500, 200), PlayBeepPattern.Tone(3_000, 200))),
-    THREE_TONES_DOWN("Three tones down", listOf(PlayBeepPattern.Tone(3_000, 200), PlayBeepPattern.Tone(2_500, 200), PlayBeepPattern.Tone(2_000, 200))),
+    THREE_TONES_UP("Three tones up", listOf(PlayBeepPattern.Tone(4_000, 500), PlayBeepPattern.Tone(5_000, 500), PlayBeepPattern.Tone(6_000, 500))),
+    THREE_TONES_DOWN("Three tones down", listOf(PlayBeepPattern.Tone(6_000, 500), PlayBeepPattern.Tone(5_000, 500), PlayBeepPattern.Tone(4_000, 500))),
     DOUBLE_HIGH("Double high", listOf(
-        PlayBeepPattern.Tone(2_200, 400),
+        PlayBeepPattern.Tone(4_000, 500),
         PlayBeepPattern.Tone(0, 200),
-        PlayBeepPattern.Tone(2_800, 200),
+        PlayBeepPattern.Tone(5_000, 500),
         PlayBeepPattern.Tone(0, 200),
-        PlayBeepPattern.Tone(2_800, 200),
+        PlayBeepPattern.Tone(5_000, 500),
         PlayBeepPattern.Tone(0, 200),
-        PlayBeepPattern.Tone(2_200, 400))
-    )
+        PlayBeepPattern.Tone(4_000, 500))
+    ),
+    DOUBLE_LOW("Double low", listOf(
+        PlayBeepPattern.Tone(5_000, 500),
+        PlayBeepPattern.Tone(0, 200),
+        PlayBeepPattern.Tone(4_000, 500),
+        PlayBeepPattern.Tone(0, 200),
+        PlayBeepPattern.Tone(4_000, 500),
+        PlayBeepPattern.Tone(0, 200),
+        PlayBeepPattern.Tone(5_000, 500))
+    ),
 }
 
 /**
